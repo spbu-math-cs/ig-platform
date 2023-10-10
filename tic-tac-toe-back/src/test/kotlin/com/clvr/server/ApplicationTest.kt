a1c32a0da0683c99de5a50992c5e15a7aa15c44a
Ivan Volkov <65076429+volivan239@users.noreply.github.com>
2023-10-10 08:19:24 +0300

    Setup gradle #8 (#15)
    * Setup gradle
    
    * Set up logging
    
    * Rename subproject to tic-tac-toe-back

---
+++ tic-tac-toe-back/src/test/kotlin/com/clvr/server/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.clvr.server
+
+import com.clvr.server.plugins.configureRouting
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
