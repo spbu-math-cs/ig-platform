52ac7a19951a143e8dd962df6e3fa86854694d7b
Nikolay Stepanov <elteammate@gmail.com>
2023-10-31 00:57:49 +0300

    Inline components

--- tic-tac-toe-web/src/pages/index.tsx
+++ tic-tac-toe-web/src/pages/index.tsx
@@ -2,12 +2,11 @@ import type {NextPage} from 'next'
 import Head from 'next/head'
 import React, {useEffect, useState} from 'react'
 import {Board} from '@/components/Board'
-import {JoinGame} from "@/components/JoinGame";
-import {ChooseMode, ColorTheme} from '@/components/ChooseMode'
 import {WinnerModal} from '@/components/WinnerModal'
-import {nextTheme, setTheme, ThemeClass} from "@/state/themeSlice"
+import {nextTheme} from "@/state/themeSlice"
 import {useDispatch} from "react-redux"
-
+import {XIcon} from "@/components/XIcon"
+import {OIcon} from "@/components/OIcon"
 
 
 const Home: NextPage = () => {
@@ -20,76 +19,14 @@ const Home: NextPage = () => {
     const [isHost, setIsHost] = useState<boolean>(true)
     const [isJoining, setIsJoining] = useState<boolean>(false)
     const [isError, setIsError] = useState<boolean>(false)
-
+    const [joiningGameId, setJoiningGameId] = useState('')
 
     const dispatch = useDispatch()
-
-    useEffect(() => {
-        if (isHost) return;
-        fetch("http://0.0.0.0:8080/api/game-session", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "quiz": {
-                    "id": "ABCD",
-                },
-            })
-        }).then((response) => {
-            return response.json()
-        }).then((data) => {
-            console.log(data)
-            setSessionId(data.session.id)
-        })
-    }, [isHost])
 
     // let winner = calculateWinner(squares)
     let winner = ""
 
-    function handleHostMode() {
-        setIsHost(true)
-        handleNewGame()
-    }
-
-
-    function handlePlayerMode() {
-        setIsHost(false)
-    }
-
-    function handleJoiningRequest(id : string) {  // TODO
-        if (id === 'Hello world') {
-            setIsError(true);
-            handleJoinGame()
-        }
-
-        else {
-            handleNewGame()
-        }
-    }
-
-    function handleJoinGame() {
-        handlePlayerMode();
-        setIsJoining(true)
-    }
-
-    function handleColorTheme() {
-        dispatch(nextTheme())
-    }
-
-    function PlayerGag() {
-    }
-
-    function handleRestartGame() {
-        setIsX(true)
-        // @ts-ignore
-        setSquares(Array(cols * rows).fill(null))
-    }
-
-
-    function handleNewGame() {
-
-
+    const handleNewGame = () => {
         setIsX(true)
         // @ts-ignore
         setSquares(Array(cols * rows).fill(null))
@@ -107,15 +44,32 @@ const Home: NextPage = () => {
 
         setSquares(squares)
 
-        setNewGame(true)
-        setIsJoining(false)
-    }
-
-    function handleQuitGame() {
-        setIsX(true)
-        // @ts-ignore
-        setSquares(Array(cols * rows).fill(null))
-        setNewGame(false)
+        if (isHost) {
+            fetch("http://0.0.0.0:8080/api/game-session", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    "quiz": {
+                        "id": "ABCD",
+                    },
+                }),
+            }).then((response) => {
+                return response.json()
+            }).then((data) => {
+                console.log(data)
+                setNewGame(true)
+                setIsJoining(false)
+                setSessionId(data.session.id)
+            }).catch(e => {
+                console.error(e)
+            })
+        } else {
+            setSessionId(joiningGameId)
+            setNewGame(true)
+            setIsJoining(false)
+        }
     }
 
     return (
@@ -133,20 +87,91 @@ const Home: NextPage = () => {
             {!newGame
                 ?
                 (!isJoining ?
-                    (<div>
-                        <ChooseMode
-                            handleJoinGame={handleJoinGame}
-                            handleHostMode={handleHostMode}
-                        />
-                        <ColorTheme
-                            handleColorTheme={handleColorTheme}
-                        />
-                    </div>)
+                        (<div>
+                            <div className="mt-10 md:mt-16 w-[500px] flex flex-col items-center justify-center mx-auto">
+                                <div className="flex rounded-xl px-6 py-2 items-center justify-center space-x-4">
+                                    <XIcon/>
+                                    <OIcon/>
+                                </div>
+                                <div
+                                    className={`flex flex-col items-center py-12 w-[700px] md:w-[450px] h-64 md:h-72 rounded-2xl  bg-panel mt-6 space-y-8 md:space-y-8`}>
+                                    <p className={`text-md text-txt uppercase font-extrabold  md:text-3xl space-y-12 `}>
+                                        SELECT MODE
+                                    </p>
+                                    <div
+                                        className="bg-gray-800 flex items-center justify-evenly h-35 rounded-2xl p-2 ">
+                                        <button onClick={() => {
+                                            setIsHost(true)
+                                            handleNewGame()
+                                        }}
+                                                className={`focus:bg-gray-300 hover:bg-[#ffe1a9] transition duration-300 ease-in flex items-center justify-center rounded-xl px-6 py-6  text-3xl md:text-4xl font-extrabold mt-1 text-hostTxt `}>
+                                            CREATE
+                                        </button>
+
+                                        <button onClick={() => {
+                                            setIsHost(false)
+                                            setIsJoining(true)
+                                        }}
+                                                className={`focus:bg-gray-300 hover:bg-[#ffe1a9] transition duration-300 ease-in flex items-center justify-center rounded-xl px-6 py-6 text-3xl md:text-4xl font-extrabold mt-1 text-playerTxt`}>
+                                            JOIN
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mt-106 md:mt-106  flex flex-col items-center justify-center mx-auto">
+                                <button onClick={() => dispatch(nextTheme())}
+                                        className={`button hover:ring-4 hover:ring-cyan-300 rounded-xl mt-8 px-6 py-3 bg-[#f3b236] hover:bg-panel`}>
+                                    Change Theme
+                                </button>
+                            </div>
+                        </div>)
                         :
-                        <JoinGame
-                            handleJoiningRequest={handleJoiningRequest}
-                            isError={isError}
-                        />
+
+                        <form onSubmit={e => {
+                            if (joiningGameId === 'Hello world') {
+                                setIsError(true)
+                                setIsHost(false)
+                                setIsJoining(true)
+                            } else {
+                                handleNewGame()
+                            }
+                            e.preventDefault()
+                        }}>
+                            <div className="mt-10 md:mt-16 w-[500px] flex flex-col items-center justofy-center mx-auto">
+                                <div className="flex rounded-xl px-6 py-2 items-center justify-center space-x-4">
+                                    <XIcon/>
+                                    <OIcon/>
+                                </div>
+                                <div
+                                    className={`flex flex-col items-center py-12 w-[700px] md:w-[450px] h-64 md:h-72 rounded-2xl bg-panel mt-4 space-y-8 md:space-y-6`}>
+                                    <p className={`text-md text-hostTxt uppercase font-extrabold  md:text-3xl space-y-12 `}>
+                                        ENTER GAME ID
+                                    </p>
+
+                                    <input
+                                        type="text"
+                                        className={`mt-1 border  w-80 h-24 rounded-xl px-2 py-3 bg-panel outline-0
+                                    text-3xl md:text-4xl font-bold  text-center text-txt outline-none `}
+                                        value={joiningGameId}
+                                        onChange={e => setJoiningGameId(e.currentTarget.value)}
+                                    />
+
+                                    <button onClick={e => setJoiningGameId(e.currentTarget.value)}
+                                            type="submit"
+                                            className={`button hover:ring-4 hover:ring-cyan-300 rounded-xl mt-8 px-6 py-3 bg-[#f3b236] hover:bg-panel`}>
+                                        START GAME
+                                    </button>
+
+                                    {isError ?
+                                        <p className={`text-center font-extrabold text-error text-2xl `}>
+                                            GAME DOESN'T EXIST
+                                        </p>
+                                        :
+                                        ""
+                                    }
+                                </div>
+                            </div>
+                        </form>
 
                 )
                 :
@@ -154,13 +179,17 @@ const Home: NextPage = () => {
                     <Board
                         playerX={isX}
                         sessionId={sessionId}
-                        handleRestartGame={handleRestartGame}
+                        handleRestartGame={() => {
+                            setIsX(true)
+                            setSquares(Array(cols * rows).fill(null))
+                        }}
                         isHost={isHost}/>
                     :
                     <Board
                         playerX={isX}
                         sessionId={sessionId}
-                        handleRestartGame={PlayerGag}
+                        handleRestartGame={() => {
+                        }}
                         isHost={isHost}
                     />)
             }
@@ -168,7 +197,11 @@ const Home: NextPage = () => {
                 winner &&
                 <WinnerModal
                     winner={winner}
-                    handleQuitGame={handleQuitGame}
+                    handleQuitGame={() => {
+                        setIsX(true)
+                        setSquares(Array(cols * rows).fill(null))
+                        setNewGame(false)
+                    }}
                     handleNewGame={handleNewGame}
                 />
             }
