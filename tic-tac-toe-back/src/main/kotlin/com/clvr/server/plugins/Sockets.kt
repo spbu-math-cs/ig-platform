6ffc102cdd3875a60bd2a374ca3220af36ebc4c5
Daniil Pavlenko <dp.3481088@gmail.com>
2023-10-30 23:44:55 +0300

    [backend] fix sockets

--- tic-tac-toe-back/src/main/kotlin/com/clvr/server/plugins/Sockets.kt
+++ tic-tac-toe-back/src/main/kotlin/com/clvr/server/plugins/Sockets.kt
@@ -21,20 +21,22 @@ import io.ktor.server.plugins.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import mu.KLogger
-import java.time.Duration
+import java.lang.Exception
 import java.util.concurrent.ConcurrentHashMap
 
 fun Application.configureSockets() {
     val logger: KLogger = logger()
 
     install(WebSockets) {
-        pingPeriod = Duration.ofSeconds(15)
-        timeout = Duration.ofSeconds(15)
-        maxFrameSize = Long.MAX_VALUE
-        masking = false
+//        pingPeriod = Duration.ofSeconds(15)
+//        timeout = Duration.ofSeconds(15)
+//        maxFrameSize = Long.MAX_VALUE
+//        masking = false
     }
     routing {
         // Leave it in order to test simple connection
@@ -58,39 +60,69 @@ fun Application.configureSockets() {
 
             logger.info { "Host $hostEndpoint connected to game $sessionId" }
 
-            launch {
-                for (event in hostChannel) {
-                    val jsonEvent: String = encodeEventToJson(event)
-                    outgoing.send(Frame.Text(jsonEvent))
-                    logger.debug { "Send event $jsonEvent to host $hostEndpoint in $sessionId game" }
+            coroutineScope {
+                launch {
+                    for (event in hostChannel) {
+                        try {
+                            val jsonEvent: String = encodeEventToJson(event)
+                            outgoing.send(Frame.Text(jsonEvent))
+                            logger.debug { "Send event $jsonEvent to host $hostEndpoint in $sessionId game" }
+                        } catch (e: Exception) {
+                            logger.error { "Failed to send event to host $hostEndpoint because of $e" }
+                            hostChannel.send(event)
+                        }
+                    }
                 }
-            }
 
-            for (frame in incoming) {
-                if (frame !is Frame.Text) {
-                    continue
+                launch {
+                    for (frame in incoming) {
+                        try {
+                            if (frame !is Frame.Text) {
+                                continue
+                            }
+
+                            val jsonEvent: String = frame.readText()
+                            logger.debug { "Receive event $jsonEvent from host $hostEndpoint in $sessionId game" }
+
+                            val event: Event<*> = decodeJsonToEvent(jsonEvent)
+                            sessionManager.handleHostEvent(event)
+                        } catch (e: Exception) {
+                            logger.error { "Failed to process event incoming frame because of error $e" }
+                        }
+                    }
                 }
 
-                val jsonEvent: String = frame.readText()
-                logger.debug { "Receive event $jsonEvent to host $hostEndpoint in $sessionId game" }
-
-                val event: Event<*> = decodeJsonToEvent(jsonEvent)
-                sessionManager.handleHostEvent(event)
+                val closeReason = closeReason.await()
+                logger.info { "Connection with host $hostEndpoint was closed because of $closeReason" }
+                cancel("Connection with host was closed because of $closeReason")
             }
         }
 
         webSocket("/ws/client/{session_id}") {
-            val sessionId: Long = call.parameters["session_id"]?.toLong() ?: throw IllegalArgumentException("failed to get session id")
+            val sessionId: Long =
+                call.parameters["session_id"]?.toLong() ?: throw IllegalArgumentException("failed to get session id")
             val clientEndpoint: String = call.request.origin.remoteAddress + ":" + call.request.origin.remotePort
             val sessionManager: SessionManager = this@configureSockets.getSessionManager(sessionId)
             val clientChannel: Channel<Event<*>> = sessionManager.registerClient(clientEndpoint)
             logger.info { "Client $clientEndpoint connected to game $sessionId" }
 
             try {
-                for (event in clientChannel) {
-                    val jsonEvent: String = encodeEventToJson(event)
-                    outgoing.send(Frame.Text(jsonEvent))
-                    logger.debug { "Send event $jsonEvent to client $clientEndpoint in $sessionId game" }
+                coroutineScope {
+                    launch {
+                        for (event in clientChannel) {
+                            try {
+                                val jsonEvent: String = encodeEventToJson(event)
+                                outgoing.send(Frame.Text(jsonEvent))
+                                logger.debug { "Send event $jsonEvent to client $clientEndpoint in $sessionId game" }
+                            } catch (e: Exception) {
+                                logger.error { "Failed to send event to client $clientEndpoint because of $e" }
+                            }
+                        }
+                    }
+
+                    val closeReason = closeReason.await()
+                    logger.info { "Connection with client $clientEndpoint was closed because of $closeReason" }
+                    cancel("Connection with client was closed because of $closeReason")
                 }
             } finally {
                 sessionManager.unregisterClient(clientEndpoint)
