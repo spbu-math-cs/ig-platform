4e5de4627b0ed041e4a7dfa71fae8fae31b641b8
Nikolay Stepanov <elteammate@gmail.com>
2023-12-02 21:32:49 +0300

    Remove description button

--- tic-tac-toe-web/src/components/QuizCard.tsx
+++ tic-tac-toe-web/src/components/QuizCard.tsx
@@ -7,34 +7,21 @@ interface QuizProps {
 }
 
 export function QuizCard({quiz, handleSelect}: QuizProps) {
-    const [details, setDetails] = useState(false)
-
     return (
         <div className="border-2 border-back w-[950px] rounded-2xl py-2 px-4 flex flex-col items-start mb-2">
             <div className="rounded-2xl flex flex-row">
                 <div className="rounded-2xl py-2 px-4 flex flex-row mb-1 space-x-30 w-[810px]">
                     <p className="font-bold text-2xl">{quiz.name}</p>
-
-                    {details ? <div>
-                            <p className="font-bold text-xl px-10">{quiz.comment}</p>
-                        </div>
-                        : ''}
+                    <div>
+                        <p className="font-bold text-xl px-10">{quiz.comment}</p>
+                    </div>
                 </div>
-                <button className="mt-3 font-extrabold text-xl text-JoinGameTxt space-30-px flex button hover:ring-4 py-2 hover:ring-cyan-300 rounded-xl px-6 bg-[#f3b236] hover:bg-panel"
+                <button className="font-extrabold text-xl text-JoinGameTxt space-30-px flex button hover:ring-4 py-2 hover:ring-cyan-300 rounded-xl px-6 bg-[#f3b236] hover:bg-panel"
                         onClick={() => handleSelect(quiz.id)}
                 >
                     PLAY!
                 </button>
             </div>
-
-            <button
-                className="px-4 bg-selectPanel font-bold rounded-l text-s text-back"
-                onClick={() => setDetails(prev => !prev)}
-            >
-                {details ? 'HIDE DETAILS' : 'SHOW DETAILS'}
-            </button>
-
-
         </div>
     )
 }
