e6dd5e47e067560dfa89f19d6bda8739a6b3ce8f
Daniil Berezin <fruuungl@gmail.com>
2023-12-14 15:56:42 +0300

    [backend] snake_case -> CamelCase in TemplateQuestion

--- clvr-back/nekahoot/src/main/kotlin/com/clvr/nk/common/Dataclasses.kt
+++ clvr-back/nekahoot/src/main/kotlin/com/clvr/nk/common/Dataclasses.kt
@@ -3,14 +3,17 @@ package com.clvr.nk.common
 import com.clvr.platform.api.Template
 import com.clvr.platform.api.TemplateHeader
 import com.clvr.platform.api.TemplateId
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class TemplateQuestion(
     val question: String,
     val answer: String,
-    val answer_description: String?,
-    val answer_options: List<String>,
+    @SerialName("answer_description")
+    val answerDescription: String?,
+    @SerialName("answer_options")
+    val answerOptions: List<String>,
     val time: Int,
 )
 
