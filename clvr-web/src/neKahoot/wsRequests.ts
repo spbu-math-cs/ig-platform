1af4b876d903a9992c2bbc0c3fa0cc9c874ff46c
arnyyyyy <garrybobcrabsburger@gmail.com>
2023-12-14 15:52:51 +0300

    some neKahoot stuff

---
+++ clvr-web/src/neKahoot/wsRequests.ts
@@ -0,0 +1,42 @@
+import {Mark} from "./types"
+
+/**
+ * A host to server request to open a question and show it to the players.
+ */
+interface RequestOpenQuestion {
+    type: "OPEN_QUESTION"
+    row: number
+    column: number
+}
+
+/**
+ * A host to server request to set a field on the board.
+ */
+interface RequestSetField {
+    type: "SET_FIELD"
+    row: number
+    column: number
+    mark: Mark
+}
+
+interface RequestShowAnswer {
+    type: "SHOW_ANSWER"
+    row: number
+    column: number
+}
+
+interface RequestShowNextHint {
+    type: "SHOW_NEXT_HINT"
+    row: number
+    column: number
+    currentHintsNum: number
+}
+
+/**
+ * A request to the game server.
+ */
+export type Request =
+    RequestOpenQuestion
+    | RequestSetField
+    | RequestShowAnswer
+    | RequestShowNextHint
