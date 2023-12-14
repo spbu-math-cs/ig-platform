08535ae9952e9b55177d8e52e182a5d3e3730691
Daniil Berezin <fruuungl@gmail.com>
2023-12-14 18:17:07 +0300

    [backend] added already answered error + test for late answer/already answered errors

--- clvr-back/nekahoot/src/test/kotlin/com/clvr/nk/NeKahootGameStateControllerTest.kt
+++ clvr-back/nekahoot/src/test/kotlin/com/clvr/nk/NeKahootGameStateControllerTest.kt
@@ -1,9 +1,10 @@
 package com.clvr.nk
 
 import com.clvr.platform.api.SessionId
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.function.ThrowingSupplier
 import kotlinx.coroutines.test.runTest
 import kotlin.time.Duration.Companion.seconds
 
@@ -37,33 +38,48 @@ class NeKahootGameStateControllerTest {
         return hostEvents.toList() to clientEvents.toMap()
     }
 
-    private fun checkEvents(
-        hostEvents: List<NeKahootResponsePayload>,
-        clientsToEvents: Map<String, List<NeKahootResponsePayload>>
-    ) {
-        val hostEventPayloads = hostEvents.map { payload ->
-            Assertions.assertDoesNotThrow(
-                ThrowingSupplier { payload }
-            )
-        }
-        val clientsToEventPayloads = clientsToEvents.mapValues { (_, events) ->
-            events.map { payload ->
-                Assertions.assertDoesNotThrow(
-                    ThrowingSupplier { payload }
-                )
-            }
+    private fun makeRequestFromClient(
+        game: GameState,
+        requestPayload: NeKahootRequestPayload,
+        clientEndpoint: String,
+    ): Pair<List<NeKahootResponsePayload>, Map<String, List<NeKahootResponsePayload>>> {
+        val controller = NeKahootGameController(game)
+        val communicator = MockCommunicator()
+        val hostChannel = communicator.hostChannel
+        val clientChannel = communicator.clientsToChannels
+        controller.handleFromClient(communicator, clientEndpoint, NeKahootRequest(SessionId("0"), requestPayload))
+
+        val hostEvents = generateSequence {
+            hostChannel.tryReceive().getOrNull()
+        }.map {
+            it.payload
         }
 
+        val clientEvents = clientChannel.mapValues { (_, channel) ->
+            generateSequence {
+                channel.tryReceive().getOrNull()
+            }.map {
+                it.payload
+            }.toList()
+        }
+
+        return hostEvents.toList() to clientEvents.toMap()
+    }
+
+    private fun checkCloseOfQuestion(
+        hostEventPayloads: List<NeKahootResponsePayload>,
+        clientsToEventPayloads: Map<String, List<NeKahootResponsePayload>>
+    ) {
         Assertions.assertEquals(2, hostEventPayloads.size)
         clientsToEventPayloads.forEach { (_, eventPayloads) ->
             Assertions.assertEquals(2, eventPayloads.size)
         }
 
-        Assertions.assertEquals("OPENED_QUESTION", hostEventPayloads[0].state)
-        Assertions.assertEquals("SHOW_QUESTION_ANSWER", hostEventPayloads[1].state)
+        Assertions.assertEquals(HostQuestionResponse.state, hostEventPayloads[0].state)
+        Assertions.assertEquals(ShowAnswerEvent.state, hostEventPayloads[1].state)
         clientsToEventPayloads.forEach { (_, events) ->
-            Assertions.assertEquals("OPENED_QUESTION", events[0].state)
-            Assertions.assertEquals("SHOW_QUESTION_ANSWER", events[1].state)
+            Assertions.assertEquals(HostQuestionResponse.state, events[0].state)
+            Assertions.assertEquals(ShowAnswerEvent.state, events[1].state)
         }
     }
 
@@ -72,13 +88,96 @@ class NeKahootGameStateControllerTest {
         val game = GameState(quiz)
         val startGameRequest = StartGameRequest()
         val (hostEvents, clientsToEvents) = makeRequest(game, startGameRequest)
-        checkEvents(hostEvents, clientsToEvents)
+        checkCloseOfQuestion(hostEvents, clientsToEvents)
 
         val questionRequest = QuestionRequest()
         val (hostEvents2, clientsToEvents2) = makeRequest(game, questionRequest)
-        checkEvents(hostEvents2, clientsToEvents2)
+        checkCloseOfQuestion(hostEvents2, clientsToEvents2)
 
         val (hostEvents3, clientsToEvents3) = makeRequest(game, questionRequest)
-        checkEvents(hostEvents3, clientsToEvents3)
+        checkCloseOfQuestion(hostEvents3, clientsToEvents3)
+    }
+
+
+    private fun checkAnswerResponse(
+        clientEndpoint: String,
+        game: GameState,
+        hostEventPayloads: List<NeKahootResponsePayload>,
+        clientsToEventPayloads: Map<String, List<NeKahootResponsePayload>>
+    ) {
+        Assertions.assertEquals(1, hostEventPayloads.size)
+        Assertions.assertNotNull(clientsToEventPayloads[clientEndpoint])
+        clientsToEventPayloads.forEach { (endpoint, eventPayloads) ->
+            when (endpoint) {
+                clientEndpoint -> Assertions.assertEquals(1, eventPayloads.size)
+                else -> Assertions.assertEquals(0, eventPayloads.size)
+            }
+        }
+
+        val payload = hostEventPayloads.single()
+        Assertions.assertEquals(HostQuestionResponse.state, payload.state)
+        payload as HostQuestionResponse
+        Assertions.assertEquals(game.getNumberOfAnswers(), payload.questionView.answered)
+
+        val clientPayload = clientsToEventPayloads[clientEndpoint]!!.single()
+        Assertions.assertEquals(ClientQuestionResponse.state, clientPayload.state)
+        clientPayload as ClientQuestionResponse
+        Assertions.assertEquals(game.getAnswerOfPlayer(clientEndpoint), clientPayload.questionView.givenAnswer)
+    }
+
+    private fun checkErrorResponse(
+        expectedException: IllegalGameActionException,
+        clientEndpoint: String,
+        hostEventPayloads: List<NeKahootResponsePayload>,
+        clientsToEventPayloads: Map<String, List<NeKahootResponsePayload>>
+    ) {
+        Assertions.assertEquals(0, hostEventPayloads.size)
+        Assertions.assertNotNull(clientsToEventPayloads[clientEndpoint])
+        clientsToEventPayloads.forEach { (endpoint, eventPayloads) ->
+            when (endpoint) {
+                clientEndpoint -> Assertions.assertEquals(1, eventPayloads.size)
+                else -> Assertions.assertEquals(0, eventPayloads.size)
+            }
+        }
+        val clientPayload = clientsToEventPayloads[clientEndpoint]!!.single()
+        Assertions.assertEquals(GameError.state, clientPayload.state)
+        clientPayload as GameError
+        Assertions.assertEquals(expectedException.message, clientPayload.message)
+    }
+
+    @Test
+    fun `answer to question logic`() = runTest {
+        val game = GameState(quiz)
+        game.startGame()
+        game.openQuestion(System.currentTimeMillis())
+        launch {
+            delay(game.getTime().toLong().seconds)
+            game.closeQuestion()
+        }
+
+        val client1 = "client-1"
+        val client1Request = AnswerRequest("opt2")
+        val client2 = "client-2"
+        val client2Request = AnswerRequest("opt1")
+        val client2RequestAttempt2 = AnswerRequest("opt2")
+        val client3 = "client-3"
+        val client3Request = AnswerRequest("opt2")
+
+
+        delay(10)
+        val (hostEvents, clientsToEvents) = makeRequestFromClient(game, client1Request, client1)
+        checkAnswerResponse(client1, game, hostEvents, clientsToEvents)
+
+        delay(533)
+        val (hostEvents2, clientsToEvents2) = makeRequestFromClient(game, client2Request, client2)
+        checkAnswerResponse(client2, game, hostEvents2, clientsToEvents2)
+
+        delay(1)
+        val (hostEvents2Attempt2, clientsToEvents2Attempt2) = makeRequestFromClient(game, client2RequestAttempt2, client2)
+        checkErrorResponse(AlreadyAnsweredException(), client2, hostEvents2Attempt2, clientsToEvents2Attempt2)
+
+        delay(500)
+        val (hostEvents3, clientsToEvents3) = makeRequestFromClient(game, client3Request, client3)
+        checkErrorResponse(LateAnswerException(), client3, hostEvents3, clientsToEvents3)
     }
 }
