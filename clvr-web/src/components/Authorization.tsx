9e770ce921b716af3c89768549fe3688b0efa5e3
arnyyyyy <garrybobcrabsburger@gmail.com>
2023-12-26 00:43:23 +0300

    dots in password input added

--- clvr-web/src/components/Authorization.tsx
+++ clvr-web/src/components/Authorization.tsx
@@ -127,11 +127,11 @@ export function LogIn({switchPage, setUser}: AuthProps) {
                                     <div>
                                         <label htmlFor="password"
                                                className="text-primary block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                        <textarea
-                                            className="bg-square text-primary bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        <input type="password"
+                                            className={` bg-square text-primary bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
                                             placeholder="cool password"
                                             onChange={e => {setLoginInfo({...loginInfo, password: e.currentTarget.value})}}
-                                        ></textarea>
+                                        ></input>
                                     </div>
                                     <button onClick={loginAction}
                                             className="text-primary w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
@@ -171,21 +171,21 @@ export function LogIn({switchPage, setUser}: AuthProps) {
                                     <div>
                                         <label htmlFor="password"
                                                className="text-primary block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                        <textarea
+                                        <input type="password"
                                             className="bg-square text-primary bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                             placeholder="cool password"
                                             onChange={e => {setLoginInfo({...loginInfo, password: e.currentTarget.value})}}
-                                        ></textarea>
+                                        ></input>
                                     </div>
                                     <div>
                                         <label htmlFor="confirm-password"
                                                className="text-primary block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
                                             password</label>
-                                        <textarea
+                                        <input type="password"
                                             className="bg-square text-primary bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                             placeholder="cool password"
                                             onChange={e => {setLoginInfo({...loginInfo, password_x2: e.currentTarget.value})}}
-                                        ></textarea>
+                                        ></input>
                                     </div>
                                     <button onClick={registerAction}
                                             className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Create
